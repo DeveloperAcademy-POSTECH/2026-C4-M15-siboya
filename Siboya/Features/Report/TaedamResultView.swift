9bbbc12ffc7d1f8e1bc0347b788f2229b7b95c05
Turto <121936896+ErinKYB@users.noreply.github.com>
2026-07-20 16:45:50 +0900

    feat: 이미지 에셋 추가 및 헤더 요소 제작

--- Siboya/Features/Report/TaedamResultView.swift
+++ Siboya/Features/Report/TaedamResultView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct TaedamResultView: View {
+    let week: Int
+    let question: String
+    let imageName: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Image("Taedam-BG")
+                .resizable()
+                .scaledToFill()
+                .ignoresSafeArea(edges: .all)
+                .opacity(0.38)
+            //HeaderContents
+            //Image
+            VStack(spacing: 12) {
+                Image("TitleImage")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 144, height: 144)
+                    .clipShape(
+                        RoundedRectangle(
+                            cornerRadius: 32,
+                            style: .continuous
+                        )
+                    )
+                    .accessibilityHidden(true)
+               //주차별
+                Text("\(week)주차")
+                    .font(.title3)
+                    .foregroundStyle(
+                        Color(red: 1.0, green: 0.39, blue: 0.37)
+                    )
+                //태담제목
+                Text(question)
+                    .font(.title2.bold())
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.primary)
+                    .fixedSize(horizontal: false, vertical: true)
+            }
+        }
     }
 }
 
 #Preview {
-    TaedamResultView()
+    TaedamResultView(week: 22, question: "일요일 아침", imageName: "TitleImage")
 }
