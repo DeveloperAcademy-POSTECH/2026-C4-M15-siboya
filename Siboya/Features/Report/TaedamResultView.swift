aeb060e1044193bdcfa4cae0fc507cb6b52495a2
Turto <121936896+ErinKYB@users.noreply.github.com>
2026-07-20 23:00:23 +0900

    feat: Header요소 분리

--- Siboya/Features/Report/TaedamResultView.swift
+++ Siboya/Features/Report/TaedamResultView.swift
@@ -13,38 +13,9 @@ struct TaedamResultView: View {
     let imageName: String
     
     var body: some View {
-        ZStack{
-            Image("Taedam-BG")
-                .resizable()
-                .scaledToFill()
-                .ignoresSafeArea(edges: .all)
-                .opacity(0.38)
-            //HeaderContents
-            //Image
-            VStack(spacing: 12) {
-                Image("TitleImage")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 144, height: 144)
-                    .clipShape(
-                        RoundedRectangle(
-                            cornerRadius: 32,
-                            style: .continuous
-                        )
-                    )
-                    .accessibilityHidden(true)
-               //주차별
-                Text("\(week)주차")
-                    .font(.title3)
-                    .foregroundStyle(
-                        Color(red: 1.0, green: 0.39, blue: 0.37)
-                    )
-                //태담제목
-                Text(question)
-                    .font(.title2.bold())
-                    .multilineTextAlignment(.center)
-                    .foregroundStyle(.primary)
-                    .fixedSize(horizontal: false, vertical: true)
+        ScrollView {
+            ZStack{
+                
             }
         }
     }
