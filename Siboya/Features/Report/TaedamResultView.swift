d53954efc1dcfe143ae9a29f982153a0e3e3d398
Turto <121936896+ErinKYB@users.noreply.github.com>
2026-07-20 16:20:42 +0900

    feat: 태담정리본 화면 파일 생성

---
+++ Siboya/Features/Report/TaedamResultView.swift
@@ -0,0 +1,18 @@
+//
+//  TaedamResultView.swift
+//  Siboya
+//
+//  Created by Erin Yaebin Kim on 7/20/26.
+//
+
+import SwiftUI
+
+struct TaedamResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaedamResultView()
+}
