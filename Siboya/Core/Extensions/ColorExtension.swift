1a8e9db14729e59a2f386c1785daab8cb056685a
Bara3545 <siwoni@ewhain.net>
2026-07-23 10:39:16 +0900

    refator: / 옆에 공백추가

--- Siboya/Core/Extensions/ColorExtension.swift
+++ Siboya/Core/Extensions/ColorExtension.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 
 extension Color{
-    static let Background = Color("Background")//배경색
-    static let TextPrimary = Color("TextPrimary")//기본 텍스트색
-    static let TextDiasbled = Color("TextDisabled")//비활성 텍스트색
+    static let Background = Color("Background")// 배경색
+    static let TextPrimary = Color("TextPrimary")// 기본 텍스트색
+    static let TextDiasbled = Color("TextDisabled")// 비활성 텍스트색
     static let Primary = Color("Primary")// 브랜드 메인색
     static let Accent = Color("accent")
     
